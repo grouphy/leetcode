5afa8ad0f6c7a069414a94dc74335b42a1b0fffc
杜凯超 <2822365215@qq.com>
2019-08-10 23:05:52 +0800

    最长公共前缀

---
+++ longestCommonPrefix0810/Solution.java
@@ -0,0 +1,35 @@
+package longestCommonPrefix0810;
+
+/**
+ * Created With IntelliJ IDEA.
+ * Descriptions:编写一个函数来查找字符串数组中的最长公共前缀。
+ *
+ * 如果不存在公共前缀，返回空字符串 ""。
+ *
+ * 示例 1:
+ *
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * 示例 2:
+ *
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ *
+ * User:Mr.Du
+ * Date:2019/8/10
+ * Time:23:04
+ */
+public class Solution {
+
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        return prefix;
+    }
+}
